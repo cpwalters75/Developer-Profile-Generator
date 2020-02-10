6ba88f56bb2e8d34688f1d3f54b29a529a184890
Chris Walters <cpwalters75@gmail.com>
2020-02-09 19:45:01 -0500

    update

--- index.js
+++ index.js
@@ -1,272 +1,65 @@
-const inquirer = require("inquirer");
 const fs = require("fs");
-const util = require("util");
-var pdf = require('html-pdf');
-const axios = require('axios');
-var html = fs.readFileSync('./index.html', 'utf8');
-var options = { format: 'Letter' }
-const writeFileAsync = util.promisify(fs.writeFile);
+const path = require("path");
+const inquirer = require("inquirer");
+const open = require("open");
+const convertFactory = require("electron-html-to");
+const api = require("./api");
+const generateHTML = require("./generateHTML");
 
-function promptUser() {
-  return inquirer.prompt([
-    {
-      type: "input",
-      name: "name",
-      message: "What is your name?"
-    },
-    {
-      type: "list",
-      name: "colors",
-      message: "Please choose a color?",
-      choices: ["Red", "Blue", "Green", "Pink"]
-    },
-    {
-      type: "input",
-      name: "github",
-      message: "Enter your GitHub Username"
-    },
-    {
-      type: "input",
-      name: "linkedin",
-      message: "Enter your LinkedIn URL."
-    }
-  ]);
-}
+const questions = [
+  {
+    type: "input",
+    name: "github",
+    message: "What is your GitHub username?"
+  },
 
-function generateHTML(answers, name, following) {
-  return `
-  <!DOCTYPE html>
-  <html lang="en">
-     <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"/>
-        <link href="https://fonts.googleapis.com/css?family=BioRhyme|Cabin&display=swap" rel="stylesheet">
-        <title>Document</title>
-        <style>
-            @page {
-              margin: 0;
-            }
-           *,
-           *::after,
-           *::before {
-           box-sizing: border-box;
-           }
-           html, body {
-           padding: 0;
-           margin: 0;
-           }
-           html, body, .wrapper {
-           height: 100%;
-           }
-           .wrapper {
-           background-color: red;
-           padding-top: 100px;
-           }
-           body {
-           background-color: white;
-           -webkit-print-color-adjust: exact !important;
-           font-family: 'Cabin', sans-serif;
-           }
-           main {
-           background-color: #E9EDEE;
-           height: auto;
-           padding-top: 30px;
-           }
-           h1, h2, h3, h4, h5, h6 {
-           font-family: 'BioRhyme', serif;
-           margin: 0;
-           }
-           h1 {
-           font-size: 3em;
-           }
-           h2 {
-           font-size: 2.5em;
-           }
-           h3 {
-           font-size: 2em;
-           }
-           h4 {
-           font-size: 1.5em;
-           }
-           h5 {
-           font-size: 1.3em;
-           }
-           h6 {
-           font-size: 1.2em;
-           }
-           .photo-header {
-           position: relative;
-           margin: 0 auto;
-           margin-bottom: -50px;
-           display: flex;
-           justify-content: center;
-           flex-wrap: wrap;
-           background-color: red;
-           color: white;
-           padding: 10px;
-           width: 95%;
-           border-radius: 6px;
-           }
-           .photo-header img {
-           width: 250px;
-           height: 250px;
-           border-radius: 50%;
-           object-fit: cover;
-           margin-top: -75px;
-           border: 6px solid white;
-           box-shadow: rgba(0, 0, 0, 0.3) 4px 1px 20px 4px;
-           }
-           .photo-header h1, .photo-header h2 {
-           width: 100%;
-           text-align: center;
-           }
-           .photo-header h1 {
-           margin-top: 10px;
-           }
-           .links-nav {
-           width: 100%;
-           text-align: center;
-           padding: 20px 0;
-           font-size: 1.1em;
-           }
-           .nav-link {
-           display: inline-block;
-           margin: 5px 10px;
-           }
-           .workExp-date {
-           font-style: italic;
-           font-size: .7em;
-           text-align: right;
-           margin-top: 10px;
-           }
-           .container {
-           padding: 50px;
-           padding-left: 100px;
-           padding-right: 100px;
-           }
-           .row {
-             display: flex;
-             flex-wrap: wrap;
-             justify-content: space-between;
-             margin-top: 20px;
-             margin-bottom: 20px;
-           }
-           .card {
-             padding: 20px;
-             border-radius: 6px;
-             background-color: red;
-             color: white;
-             margin: 20px;
-           }
-           
-           .col {
-           flex: 1;
-           text-align: center;
-           }
-           a, a:hover {
-           text-decoration: none;
-           color: inherit;
-           font-weight: bold;
-           }
-           @media print { 
-            body { 
-              zoom: .75; 
-            } 
-           }
-        </style>
-     </head>
-     <body>
-        <div class="wrapper">
-           <div class="photo-header">
-              
-              <h1>Hi!</h1>
-              <h2>
-              My name is ${name}</h1>
-              <h5></h5>
-              <nav class="links-nav">
-                 
-              </nav>
-           </div>
-           <main>
-              <div class="container">
-              <div class="row">
-                 <div class="col">
-                    <h3></h3>
-                 </div>
-                 </div>
-                 <div class="row">
-                 <div class="col">
-                    <div class="card">
-                      <h3>Public Repositories</h3>
-                      <h4></h4>
-                    </div>
-                 </div>
-                  <div class="col">
-                  <div class="card">
-                    <h3>Followers</h3>
-                    <h4></h4>
-                  </div>
-                 </div>
-                 </div>
-                 <div class="row">
-                 <div class="col">
-                 <div class="card">
-                    <h3>GitHub Stars</h3>
-                    <h4></h4>
-                    </div>
-                 </div>
-                  <div class="col">
-                  <div class="card">
-                    <h3>Following</h3>
-                    <h4></h4>
-                    </div>
-                 </div>
-                 </div>
-              </div>
-           </main>
-        </div>
-     </body>
-  </html>`;
-}
-// need to make axios call 
-async function init() {
-  console.log("hi")
-  try {
-    const answers = await promptUser();
-    
-
-
-    axios.get(`https://api.github.com/users/${answers.github}`).then(function (response) {
-        let name = response.data.name;
-        let following  = respoinse.data.following;
-        let html = generateHTML(answers, name, following);
-
-        writeFileAsync("index.html", html);
-        console.log(name);
-    }).catch(function (err){
-        console.log(error);
-    });
-
-    
-
-    
-
-
-    
-
-
-    pdf.create(html, options).toFile('./resume.pdf', function(err, res) {
-      if (err) return console.log(err);
-      console.log(res); // { filename: '/app/businesscard.pdf' }
-    });
-    
-
-    console.log("Successfully wrote to index.html");
-  } catch(err) {
-    console.log(err);
+  {
+    type: "list",
+    name: "color",
+    message: "What is your favorite color?",
+    choices: ["red", "blue", "green", "pink"]
   }
+];
+
+function writeToFile(fileName, data) {
+  return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
+
+function init() {
+  inquirer.prompt(questions).then(({ github, color }) => {
+    console.log("Searching...");
+
+    api
+      .getUser(github)
+      .then(response =>
+        api.getTotalStars(github).then(stars => {
+          return generateHTML({
+            stars,
+            color,
+            ...response.data
+          });
+        })
+      )
+      .then(html => {
+        const conversion = convertFactory({
+          converterPath: convertFactory.converters.PDF
+        });
+
+        conversion({ html }, function(err, result) {
+          if (err) {
+            return console.error(err);
+          }
+
+          result.stream.pipe(
+            fs.createWriteStream(path.join(__dirname, "resume.pdf"))
+          );
+          conversion.kill();
+        });
+
+        open(path.join(process.cwd(), "resume.pdf"));
+      });
+  });
 }
 
 init();
+
